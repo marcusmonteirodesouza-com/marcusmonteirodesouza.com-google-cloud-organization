d1b3fa3ccf16dd7b8c31232c82f23792f326b37c
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2024-07-17 18:54:30 -0400

    Grant 'roles/iam.serviceAccountUser' on the cloud build service account to the projects' admins group

--- infra/terraform/org/modules/projects/modules/project/admins_group.tf
+++ infra/terraform/org/modules/projects/modules/project/admins_group.tf
@@ -3,6 +3,11 @@ locals {
     "roles/cloudbuild.builds.editor",
     "roles/viewer",
   ]
+
+  admins_group_cloudbuild_sa_roles = [
+    "roles/iam.serviceAccountTokenCreator",
+    "roles/iam.serviceAccountUser"
+  ]
 }
 
 resource "google_project_iam_member" "admins_group" {
@@ -16,4 +21,11 @@ resource "google_storage_bucket_iam_member" "admins_group_tfstate" {
   bucket = google_storage_bucket.tfstate.name
   role   = "roles/storage.objectUser"
   member = "group:${var.admins_group_email}"
+}
+
+resource "google_service_account_iam_member" "admins_group_cloudbuild_sa" {
+  for_each           = toset(local.admins_group_cloudbuild_sa_roles)
+  service_account_id = google_service_account.cloudbuild.id
+  role               = each.value
+  member             = "group:${var.admins_group_email}"
 }
