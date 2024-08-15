4d5990bc409f99b5ef3364ea41711e2a4687f670
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2024-08-14 21:23:05 -0400

    Add AI Job Apply project

--- infra/terraform/deploy/modules/base_env/modules/projects/ai_job_apply.tf
+++ infra/terraform/deploy/modules/base_env/modules/projects/ai_job_apply.tf
@@ -1,26 +1,32 @@
 locals {
-  homepage_project_display_name_prefix = "Home Page"
+  ai_job_apply_project_display_name_prefix = "AI Job Apply"
 
-  homepage_project_naming_description = "homepage"
+  ai_job_apply_project_naming_description = "aijobapply"
 }
 
-module "homepage_production" {
+module "ai_job_apply_production" {
   source = "./modules/project"
 
-  admins_group_email = "homepage-administrators-production@${data.google_organization.org.domain}"
+  admins_group_email = "ai-job-apply-administrators-production@${data.google_organization.org.domain}"
   billing_account    = var.billing_account
   cloudbuild_sa_project_roles = [
     "roles/run.admin",
   ]
-  display_name = "${local.homepage_project_display_name_prefix} - Production"
+  display_name = "${local.ai_job_apply_project_display_name_prefix} - Production"
   enable_services = [
+    "aiplatform.googleapis.com",
+    "run.googleapis.com"
   ]
   environment        = var.environment
   folder_id          = var.folder_production_id
   is_shared_vpc_host = false
-  naming_description = local.homepage_project_naming_description
+  naming_description = local.ai_job_apply_project_naming_description
   org_id             = var.org_id
   service_accounts = {
+    api-server = [
+      "roles/aiplatform.user",
+      "roles/logging.logWriter"
+    ]
     web-frontend = [
       "roles/logging.logWriter"
     ]
